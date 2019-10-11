b09d59fcac8ab118b6fd56c69143baf0f265857c
Stefan Neufeind <github@stefan-neufeind.de>
2019-10-11 17:17:49 +0200

    Add SQL-definition for table tx_mksamlauth_domain_model_serviceprovider
    Table was missing in ext_tables.sql

--- ext_tables.sql
+++ ext_tables.sql
@@ -24,6 +24,28 @@ CREATE TABLE tx_mksamlauth_domain_model_identityprovider (
     KEY parent (pid)
 );
 
+CREATE TABLE tx_mksamlauth_domain_model_serviceprovider (
+    uid int(11) NOT NULL auto_increment,
+    pid int(11) DEFAULT '0' NOT NULL,
+    tstamp int(11) DEFAULT '0' NOT NULL,
+    crdate int(11) DEFAULT '0' NOT NULL,
+    cruser_id int(11) DEFAULT '0' NOT NULL,
+    sorting int(10) DEFAULT '0' NOT NULL,
+    deleted tinyint(4) DEFAULT '0' NOT NULL,
+    hidden tinyint(4) DEFAULT '0' NOT NULL,
+    starttime int(11) DEFAULT '0' NOT NULL,
+    endtime int(11) DEFAULT '0' NOT NULL,
+
+    name VARCHAR(255) DEFAULT '' NOT NULL,
+    domain VARCHAR(255) NOT NULL,
+    certificate text NOT NULL,
+    cert_key text NOT NULL,
+    passphrase VARCHAR(255) NULL,
+
+    PRIMARY KEY (uid),
+    KEY parent (pid)
+);
+
 CREATE TABLE tx_mksamlauth_domain_model_group_mapping (
   uid int(11) NOT NULL auto_increment,
   pid int(11) DEFAULT '0' NOT NULL,
