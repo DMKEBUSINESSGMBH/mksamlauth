735e8a97f81167729cdc93ddb41ba96989b2075d
Markus Bachmann <markus.bachmann@bachi.biz>
2020-04-09 00:44:28 +0200

    Fix version constraints

--- ext_emconf.php
+++ ext_emconf.php
@@ -31,7 +31,7 @@ $EM_CONF[$_EXTKEY] = [
     'CGLcompliance_note' => '',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-9.5.99',
+            'typo3' => '8.7.0-8.7.99',
         ],
         'conflicts' => [],
         'suggests' => []
